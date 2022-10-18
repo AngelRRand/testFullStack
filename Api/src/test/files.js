a8724b463c07a17c2ef9dc1707a5283fc7bf81fb
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-10-18 15:06:33 -0300

    new dependencies

--- Api/src/test/files.js
+++ Api/src/test/files.js
@@ -1,2 +1,15 @@
 let chai = require('chai')
+let chaiHttp = require('chai-http')
 let server = require('../app.js')
+
+
+chai.should()
+chai.use(chaiHttp)
+
+
+describe('Files', () => {
+    /* 
+    Test GET
+    */
+   
+})
