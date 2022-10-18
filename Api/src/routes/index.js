fe66638c90a7963da2268a14bdb3260fd086d35f
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-10-18 13:20:10 -0300

    fixed route

--- Api/src/routes/index.js
+++ Api/src/routes/index.js
@@ -1,14 +1,25 @@
+const express = require('express');
 const { Router } = require('express');
 const router = Router();
 const { getApiFiles, getFiles } = require('../controllers');
+router.use(express.json())
+
+
 router.get('/', async (req, res) => {
 
+    const test = req.params.test
+    
     try {
-        const apiInfo = await getFiles('test9.csv')
-        res.status(200).send(apiInfo)
+        if(test){
+            const apiInfo = await getFiles(test)
+            return res.status(200).json(apiInfo)
+        } else{
+            const testsApi = await getApiFiles()
+            return res.status(200).json(testsApi)
+        }
     } catch (error) {
         console.log(error)
-        res.status(400)
+        return res.status(400)
     }
 
 })
